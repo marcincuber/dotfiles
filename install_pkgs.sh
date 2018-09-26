fd339d74bc90a1588e65ddfb24eef0f22b892115
Marcin Cuber <marcincuber@macbook-pro.home>
2018-09-26 20:04:42 +0100

    remove rvm and small fix

--- install_pkgs.sh
+++ install_pkgs.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Pull repository to Documents.
-git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/Documents
+git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/Documents/bash-it
 
 # Install brew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
@@ -9,5 +9,3 @@ git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/Documents
 # Install nvm
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
 
-# Install ruby
-curl -sSL https://get.rvm.io | bash -s stable --ruby
